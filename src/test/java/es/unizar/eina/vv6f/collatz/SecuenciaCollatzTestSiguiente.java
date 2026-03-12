2736ba0b40756078391940ad3150f7b0a2dceb39
Miguel Ángel Latre Abadía <latre@unizar.es>
2026-03-12 19:57:11 +0100

    Add parameterized tests to `siguienteCollatz`

--- src/test/java/es/unizar/eina/vv6f/collatz/SecuenciaCollatzTestSiguiente.java
+++ src/test/java/es/unizar/eina/vv6f/collatz/SecuenciaCollatzTestSiguiente.java
@@ -1,6 +1,9 @@
 package es.unizar.eina.vv6f.collatz;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,15 +16,37 @@ import static es.unizar.eina.vv6f.collatz.SecuenciaCollatz.siguienteCollatz;
 
 public class SecuenciaCollatzTestSiguiente {
 
+    @ParameterizedTest(name="siguienteCollatz({0}) es {1}")
+    @CsvSource({
+            "40, 20",
+            "13, 40",
+            "20, 10",
+            "10,  5",
+             "5, 16",
+            "16, 8",
+             "8, 4",
+             "4, 2",
+             "2, 1",
+    })
+    public void siguienteCollatzCsvTest(long input, long expected) {
+        assertEquals(expected, siguienteCollatz(input));
+    }
+
+    @ParameterizedTest(name="siguienteCollatz({0}) es {1}")
+    @CsvFileSource(resources = "/siguiente.csv", numLinesToSkip = 1)
+    public void siguienteCollatzCsvFileTest(long input, long expected) {
+        assertEquals(expected, siguienteCollatz(input));
+    }
+
     @TestFactory
-    Collection<DynamicTest> testsSecuenciasCollatzSiguiente() {
+    Collection<DynamicTest> siguienteCollatzDynamicTest() {
         List<DynamicTest> tests = new ArrayList<>();
         for (int i = 0; i < SECUENCIA_EJEMPLO.length - 1; i++) {
             final int numTest = i;
             String nombreTest = String.format("siguienteCollatz(%d) es %d",
                     SECUENCIA_EJEMPLO[numTest], SECUENCIA_EJEMPLO[numTest + 1]);
             tests.add(dynamicTest(nombreTest,
-                    () -> Assertions.assertEquals(SECUENCIA_EJEMPLO[numTest + 1],
+                    () -> assertEquals(SECUENCIA_EJEMPLO[numTest + 1],
                             siguienteCollatz(SECUENCIA_EJEMPLO[numTest]))));
         }
         return tests;
